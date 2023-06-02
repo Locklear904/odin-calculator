250e358c0cd4b326a241b8f4cda05809492ca48d
Rich Barnes <locklear904@gmail.com>
2023-06-02 09:57:01 -0400

    Draft basic calculator functions and variables
    Create functions for the basic operations, variables to hold the
    values used, and an operation function to glue them together.

---
+++ javascript.js
@@ -0,0 +1,33 @@
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
+let firstNum
+let secondNum
+let operator
+
+function operate(a, b, c){
+    if (b === `+`) {
+        add(a, c);
+    }   else if (b === `-`) {
+        subtract(a, c);
+    }   else if (b === `*`) {
+        multiply(a, c);
+    }   else if (b === `/`){
+        divide(a, c);
+    }   else {
+        alert("Please try again!");
+    }
+}
