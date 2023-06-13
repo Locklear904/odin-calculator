9daa7bf6c3714f6cd013ef7229abfd6c00873f73
Rich Barnes <locklear904@gmail.com>
2023-06-13 10:52:01 -0400

    Prototype basic calculator javascript
    Create functional calculator. Its current use only allows for two
    numbers, an operator, and equals evaluates. Will reevaluate to
    properly allow for multiple operations in one calculation.

--- javascript.js
+++ javascript.js
@@ -1,39 +1,60 @@
-function add(a, b){
-    return a + b;
-}
 
-function subtract(a, b){
-    return a - b;
-}
-
-function multiply(a, b){
-    return a * b;
-}
-
-function divide(a, b){
-    return a / b;
-}
-
-let firstNum
-let secondNum
-let operator
+let firstNum = ""
+let secondNum = ""
+let operator = ""
 
 function operate(a, b, c){
-    if (b === `+`) {
-        add(a, c);
-    }   else if (b === `-`) {
-        subtract(a, c);
-    }   else if (b === `*`) {
-        multiply(a, c);
-    }   else if (b === `/`){
-        divide(a, c);
+    if (b == "+") {
+        return a + c;
+    }   else if (b == "-") {
+        return a - c;
+    }   else if (b == "x") {
+        return a * c;
+    }   else if (b == "/"){
+        return a / c;
     }   else {
         alert("Please try again!");
     }
 }
 
-const display = document.querySelector('#calcDisplay');
+let display = document.querySelector('#calcDisplay');
+const calcButtons = document.querySelectorAll('.calcBtn');
+const calcOpBtns = document.querySelectorAll('.calcOpBtn');
+const EqBtn = document.querySelector('#calcEqBtn');
+const clrBtn = document.querySelector('#clrBtn');
+
+function NumListeners(button){
+    button.addEventListener("click", displayText);
+}
+
+function OpListeners(button){
+    button.addEventListener("click", displayOperator);
+}
 
 function displayText(){
-
+    display.textContent += event.target.textContent;
 }
+
+function displayOperator(){
+    firstNum = Number(display.textContent);
+    display.textContent = "";
+    operator = event.target.textContent;
+}
+
+function equals(){
+    secondNum = Number(display.textContent);
+    display.textContent = operate(firstNum, operator, secondNum);
+}
+
+function clear(){
+    display.textContent = "";
+    firstNum = "";
+    secondNum = "";
+    operator = "";
+}
+
+clrBtn.addEventListener("click", clear);
+EqBtn.addEventListener("click", equals)
+calcButtons.forEach(NumListeners);
+calcOpBtns.forEach(OpListeners);
+
